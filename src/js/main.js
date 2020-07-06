05abaa83a08bb0fb870474be61bb607d04bb3b09
mariajosegomezcampos <mariajosegomezcampo@gmail.com>
2020-07-06 17:27:17 -0400

    añado funcion al slider

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,32 @@
-$(".button-scroll").click(function() {
-    let name = $(this).data("name")
+ 
+ //Creamos un evento click para el enlace
+ $(".button-scroll").click(function() {
+  //Creamos el string del enlace ancla
+     let name = $(this).data("name")
+
     let box = "#" + name + "-box"
+        //Funcionalidad de scroll lento para el enlace ancla en 3 segundos
     $('html, body').animate({
+
     scrollTop: $(box).offset().top
     }, 2000);
-  });
+ });
+
+
+
+  //Creamos un evento click para el enlace
+$('.btn-ancla').click(function(){
+      //Anulamos la funcionalidad por defecto del evento
+      evento.preventDefault();
+      let codigo = "#" + $(this).data("ancla");
+      //Funcionalidad de scroll lento para el enlace ancla en 3 segundos
+      $("html,body").animate({scrollTop: $(codigo).offset().top}, 2000);
+
+});
   
-  $('.slider-flex').slick({});
+
+
+
+   $('.section-slider-desktop').slick({
+     arrows: false
+   });
