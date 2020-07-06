cf75d8e010e73d418af50cf2f08afdcd330b77cf
mariajosegomezcampos <mariajosegomezcampo@gmail.com>
2020-07-06 18:03:47 -0400

    añado funcion scroll ancla

--- src/js/main.js
+++ src/js/main.js
@@ -1,31 +1,17 @@
  
- //Creamos un evento click para el enlace
- $(".button-scroll").click(function() {
-  //Creamos el string del enlace ancla
-     let name = $(this).data("name")
 
-    let box = "#" + name + "-box"
-        //Funcionalidad de scroll lento para el enlace ancla en 3 segundos
-    $('html, body').animate({
-
-    scrollTop: $(box).offset().top
-    }, 2000);
- });
-
-
-
-  //Creamos un evento click para el enlace
-$('.btn-ancla').click(function(){
-      //Anulamos la funcionalidad por defecto del evento
-      evento.preventDefault();
-      let codigo = "#" + $(this).data("ancla");
-      //Funcionalidad de scroll lento para el enlace ancla en 3 segundos
-      $("html,body").animate({scrollTop: $(codigo).offset().top}, 2000);
-
+// funcion scroll ancla 
+ $('nav a').click(function(e){ //Creamos un evento click para el enlace			
+   e.preventDefault();		//evitar el eventos del enlace normal
+   var strAncla=$(this).attr('href'); //id del ancla
+      $('body,html').stop(true,true).animate({				
+         scrollTop: $(strAncla).offset().top
+      },2000);
+   
 });
-  
 
 
+//funcion libreria slick
 
    $('.section-slider-desktop').slick({
      arrows: false
