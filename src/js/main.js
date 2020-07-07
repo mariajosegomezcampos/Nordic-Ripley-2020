13f0a4b83570209cf4d861583dfdd1784fd64382
mariajosegomezcampos <mariajosegomezcampo@gmail.com>
2020-07-07 17:20:36 -0400

    cambios css

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,7 @@
 // funcion scroll ancla 
  $('nav a').click(function(e){ //Creamos un evento click para el enlace			
    e.preventDefault();		//evitar el eventos del enlace normal
-   var strAncla=$(this).attr('href'); //id del ancla
+   let strAncla=$(this).attr('href'); //id del ancla - le agrego un atributo mediant attr
       $('body,html').stop(true,true).animate({				
          scrollTop: $(strAncla).offset().top
       },2000);
@@ -11,8 +11,13 @@
 });
 
 
+
+// codigo carla 
+
+// s
+
 //funcion libreria slick
 
-   $('.section-slider-desktop').slick({
+   $('.section-slider').slick({
      arrows: false
    });
